ffe44804d106f8d883c73d3be67a3c8d11166040
Adhulka <Adhulkanand@gmail.com>
2023-04-04 09:34:30 +0530

    commit

--- bin/program2.dart
+++ bin/program2.dart
@@ -1,9 +1,9 @@
 void main() {
-  for (int i = 1; i <= 20; i++) {
-    if (i % 2 == 0) {
-      print(i);
-    }
-  }
+//   for (int i = 1; i <= 20; i++) {
+//     if (i % 2 == 0) {
+//       print(i);
+//     }
+//   }
 
 //   for (int i = 1; i <= 20; i++) {
 //     if (i % 2 == 0) {
@@ -11,4 +11,10 @@ void main() {
 //     }
 //     print(i);
 //   }
+  int i = 1;
+  for (i = 1; i <= 25; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      print(i);
+    }
+  }
 }
