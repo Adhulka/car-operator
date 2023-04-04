bd16fcec4bed71dd4686df79b8bf976b3d6d30f0
Adhulka <Adhulkanand@gmail.com>
2023-04-04 11:29:05 +0530

    COMMIT

---
+++ bin/function.dart
@@ -0,0 +1,41 @@
+void main() {
+  display("abu", 8857599849);
+  display("arun", 8157664476, 88477466773);
+  display("abi", 55556664444, 3343334555, "kerala");
+  show("vinu", 7746363773, phone2: 76685656765, state: "kerala");
+  show("vinu", 7746363773, state: "kerala");
+  show2("aana", 77468383738, state: "kerala");
+  display2("AD", 4445543225, state: "KERSLA", pincode: 9877877);
+}
+
+///optional positiona
+void display(String name, int phone, [int? phone2, String? state]) {
+  print("name:$name");
+  print("phone:$phone");
+  print("phone2:$phone2");
+  print("state:$state");
+}
+
+///optional named parameterised function
+void show(String name, int phone, {int? phone2, String? state}) {
+  print("name:$name");
+  print("phone:$phone");
+  print("phone2:$phone2");
+  print("state:$state");
+}
+
+void show2(String name, int phone, {int? phone2, required String state}) {
+  print("name:$name");
+  print("phone:$phone");
+  print("phone2:$phone2");
+  print("state:$state");
+}
+
+void display2(String name, int phone,
+    {int? phone2, required String state, int pincode = 680505}) {
+  print("name:$name");
+  print("phone:$phone");
+  print("phone2:$phone2");
+  print("state:$state");
+  print("pin:$pincode");
+}
